c25771bcb7b73232fb1559fd947ab244547ad593
yulduz <yulduz83@gmail.com>
2021-05-01 12:33:42 -0500

    handles sign up on client side

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -33,7 +33,7 @@ const LoginForm = () => {
       });
       Auth.login(data.login.token);
     } catch (err) {
-      console.error(err);
+      console.error(error);
       setShowAlert(true);
     }
 
