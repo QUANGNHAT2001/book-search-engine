928d52120f377edcc2b2e3c923b3c148c7c1af62
yulduz <yulduz83@gmail.com>
2021-05-01 00:10:46 -0500

    implements me and sign up apis

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,32 @@
-// import {gql} = require ('apoll')
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+  type User {
+    username: String!
+    email: String!
+    savedBoooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
