3208784bfc7c100b461c831b433d86552164a56f
QUANGNHAT2001 <102568208+QUANGNHAT2001@users.noreply.github.com>
2022-04-10 23:45:45 +0700

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,8 @@
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query) => {
-  return fetch(``);
+  return fetch(`bookId="1"
+  image=""
+  link="https://www.googleapis.com/books/v1/volumes?q=harry+potter"
+  tittle = "ccc"`);
 };
